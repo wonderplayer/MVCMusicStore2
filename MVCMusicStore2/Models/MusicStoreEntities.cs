5265d087ea8bdfc37e29b51fee8d88705e23f199
Rolands Strakis <roland.strakis@gmail.com>
2016-04-20 14:04:33 +0300

    Part 5

--- MVCMusicStore2/Models/MusicStoreEntities.cs
+++ MVCMusicStore2/Models/MusicStoreEntities.cs
@@ -7,5 +7,6 @@ namespace MVCMusicStore2.Models
         public DbSet<Album> Albums { get; set; }
         public DbSet<Genre> Genres { get; set; }
 
+        public System.Data.Entity.DbSet<MVCMusicStore2.Models.Artist> Artists { get; set; }
     }
 }
